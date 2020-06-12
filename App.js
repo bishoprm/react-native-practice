c57bdefbb0825f546f2432cf3e434328cefeb3d3
bishoprm <bishop.rose@gmail.com>
2020-06-12 17:31:33 +0200

    flexbox styling with boxes in the middle

--- App.js
+++ App.js
@@ -3,12 +3,41 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ height: "100%", flexDirection: "row" }}>
+    <View
+      style={{
+        height: "100%",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "stretch",
+      }}
+    >
       <View
-        style={{ flex: 1, width: "20%", backgroundColor: "rebeccapurple" }}
+        style={{
+          flex: 1,
+          width: "20%",
+          alignSelf: "center",
+          height: "20%",
+          backgroundColor: "rebeccapurple",
+        }}
       />
-      <View style={{ flex: 2, width: "50%", backgroundColor: "crimson" }} />
-      <View style={{ flex: 3, width: "80%", backgroundColor: "gold" }} />
+      <View
+        style={{
+          flex: 2,
+          width: "50%",
+          alignSelf: "center",
+          height: "50%",
+          backgroundColor: "crimson",
+        }}
+      />
+      <View
+        style={{
+          flex: 3,
+          width: "80%",
+          alignSelf: "center",
+          height: "80%",
+          backgroundColor: "gold",
+        }}
+      />
     </View>
   );
 }
